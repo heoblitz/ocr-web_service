3d08fa64218d3f894b5cbaaece113f0135de8186
wone2287 <wone2287@gmail.com>
2019-11-26 01:19:57 +0900

    fix app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+#import argv
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,8 +11,11 @@ def index():
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(f.filename)
+        file_name = secure_filename(f.filename) # 보안 요소 
+        f.save(file_name)                       # 파일 이름에 코드 삽입 금지.
+        print(file_name)
         return '파일 업로드 성공'
+
     elif request.method == 'GET':
         return 'not allow'
 
