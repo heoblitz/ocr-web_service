0063db8dc37f7fc571771307b0fe9d1d2c4a184e
wone2287 <wone2287@gmail.com>
2019-12-06 02:16:33 +0900

    adit html

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from flask import Flask, render_template, request
+from flask import redirect, url_for
 from werkzeug.utils import secure_filename
 #import argv
 app = Flask(__name__)
 
-@app.route('/')
+a = 'a'
+
+@app.route('/', methods = ['GET'])
 def index():
-    return render_template('index.html')
+    global a
+    return render_template('index.html', output=a)
 
 @app.route('/upload', methods =['GET', 'POST'])
 def upload_file():
+    global a
     if request.method == 'POST':
+        a = 'changed'
         f = request.files['file']
         file_name = secure_filename(f.filename) # 보안 요소 
         f.save(file_name)                       # 파일 이름에 코드 삽입 금지.
         print(file_name)
-        return '파일 업로드 성공'
+        return redirect(url_for('index')) 
 
     elif request.method == 'GET':
         return 'not allow'
