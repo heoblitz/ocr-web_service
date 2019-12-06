8b4dac855a6736cc9d45a6d72f1338df952e566a
wone2287 <wone2287@gmail.com>
2019-12-06 11:27:03 +0900

    edit ajax and text-box

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
 from flask import Flask, render_template, request
 from flask import redirect, url_for
 from werkzeug.utils import secure_filename
+import sys # print log 찍기용
 #import argv
 app = Flask(__name__)
 
-a = 'a'
-
 @app.route('/', methods = ['GET'])
 def index():
-    global a
-    return render_template('index.html', output=a)
+    ocr_data = request.args.get('ocr_data')
+    return render_template('index.html', output=ocr_data)
 
 @app.route('/upload', methods =['GET', 'POST'])
 def upload_file():
-    global a
     if request.method == 'POST':
         a = 'changed'
         f = request.files['file']
         file_name = secure_filename(f.filename) # 보안 요소 
         f.save(file_name)                       # 파일 이름에 코드 삽입 금지.
         print(file_name)
-        return redirect(url_for('index')) 
+        return redirect(url_for('tt')) 
+        #return redirect(url_for('test', code=302, ocr_data='hello')) 
 
     elif request.method == 'GET':
         return 'not allow'
+
+@app.route('/test')
+def tt():
+    return "test page"
 
 if __name__ == '__main__':
     app.run(debug = True)
